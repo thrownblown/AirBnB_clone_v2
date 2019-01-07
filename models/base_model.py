1432db00bf457afe295bb3bdf4d46fd8b90f29ca
leinefran <penny.leine@gmail.com>
2019-01-07 00:31:21 +0000

    Added: if not kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        else:
+        if not kwargs or id not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
