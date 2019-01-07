a359b063f520fbffd64d5bd84ff6bb4f4b723770
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-06 18:56:53 -0800

    id only

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,11 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-        if not kwargs or id not in kwargs:
+                if id not in kwargs:
+                    self.id = str(uuid.uuid4())
+                    self.created_at = self.updated_at = datetime.now()
+                    models.storage.new(self)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             models.storage.new(self)
