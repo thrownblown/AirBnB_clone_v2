2b669791d70fa108dfe9a4358af4d3217e4929d4
leinefran <penny.leine@gmail.com>
2019-01-08 01:33:58 +0000

    Updated attributes

--- models/place.py
+++ models/place.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This is the place class"""
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from models.base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class Place(BaseModel):
@@ -18,14 +20,19 @@ class Place(BaseModel):
         longitude: longitude in float
         amenity_ids: list of Amenity ids
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = 'places'
+    city_id = Column('city_id', String(60), nullable=False,
+                     ForeignKey('cities.id'))
+    user_id = Column('user_id', String(60), nullable=False,
+                     ForeignKey('users.id'))
+    name = Column('name', String(128), nullable=False)
+    description = Column('description', String(1024))
+    number_rooms = Column('number_rooms', Integer, default=0, nullable=False)
+    number_bathrooms = Column('number_bathroom', Integer, default=0,
+                              nullable=False)
+    max_guest = Column('max_guest', Integer, default=0, nullable=False)
+    price_by_night = Column('price_by_night', Integer, default=0,
+                            nullable=False)
+    latitude = Column('latitude', Float, default=0, nullable=False)
+    longitude = Column('latitude', Float, default=0, nullable=False)
     amenity_ids = []
