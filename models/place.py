3c9166dd03eebf1c701ed8c3fc474ddb52a3ed1c
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-08 10:47:02 -0800

    getters

--- models/place.py
+++ models/place.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python3
 """This is the place class"""
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+import os
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
+
+metadata = Base.metadata
+place_amenities = Table('association', metadata,
+                Column('place_id',
+                        String(60),
+                        primary_key=True,
+                        nullable=False),
+                Column('amenity_id',
+                        String(60),
+                        ForeignKey("amenities.id"),
+                        primary_key=True,
+                        nullable=False)
+                )
+# metadata.create_all()
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -22,25 +38,27 @@ class Place(BaseModel, Base):
     """
     __tablename__ = 'places'
     city_id = Column('city_id', String(60),
-                     ForeignKey('cities.id'), nullable=False)
+                    ForeignKey('cities.id'), nullable=False)
     user_id = Column('user_id', String(60),
-                     ForeignKey('users.id'), nullable=False)
+                    ForeignKey('users.id'), nullable=False)
     name = Column('name', String(128), nullable=False)
     description = Column('description', String(1024))
     number_rooms = Column('number_rooms', Integer, default=0, nullable=False)
     number_bathrooms = Column('number_bathroom', Integer, default=0,
-                              nullable=False)
+                    nullable=False)
     max_guest = Column('max_guest', Integer, default=0, nullable=False)
     price_by_night = Column('price_by_night', Integer, default=0,
                             nullable=False)
     latitude = Column('latitude', Float, default=0, nullable=False)
     longitude = Column('longitude', Float, default=0, nullable=False)
-    amenity_ids = []
+    place_amenity = relationship("Amenity", secondary=place_amenities,
+                            back_populates='places', viewonly=False)
 
-    metadata = Base.metadata
-    place_amenity = Table('association', metadata, Column('place_id',
-                                                          String(60),
-                                                          primary_key=True,
-                                                          nullable=False)),
-    Column('amenity_id', String(60), ForeignKey="amenities.id",
-           primary_key=True, nullable=False)))
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def amenities(self):
+            """ amenities getter for FS
+            """
+            objs = storage.all(City)
+            return ([c for c in objs if c.state_id == self.id])
+
--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
 class State(BaseModel, Base):
@@ -12,4 +14,12 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state")
+    else:
+        @property
+        def cities(self):
+            """ cities getter from FS
+            """
+            objs = storage.all(City)
+            return ([c for c in objs if c.state_id == self.id])
