9870f470eceb79c56e1c2a8f6f3912bf22b0ccb3
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-09 14:48:00 -0800

    type check console

--- console.py
+++ console.py
@@ -44,11 +44,15 @@ class HBNBCommand(cmd.Cmd):
             my_list = line.split(" ")
             clss = my_list[0]
             my_list = my_list[1:]
-
             for item in my_list:
-                item = item.split('=')
-                item[1].replace('_', ' ')
-                item = '='.join(item)
+                item = item.split("=")
+                if item[1][1] == '"' and item[1][-1] == '"':
+                    if '"' in item[1]:
+                        item[1] = item[1][1:-1]
+                        item[1].replace('"', '\"')
+                        item[1] = '"{}"'.format(item[1])
+                    item[1].replace("_", " ")
+                    item = "=".join(item)
 
             my_list = ", ".join(my_list)
             obj = eval("{}({})".format(clss, my_list))
