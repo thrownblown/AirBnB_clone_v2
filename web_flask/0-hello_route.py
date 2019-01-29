ba21964405c14c7fdbe2d04b54f5c191cff99ed9
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-29 13:21:58 -0800

    no flask run

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-
 @app.route("/")
 def hello():
     """ Well Hello There """
     return "Hello HBNB!"
+
+
+app.run("0.0.0.0")
