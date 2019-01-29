afb0c3550281e9157c9d5bbf88a2427c58ae97f1
Alex Farioletti <gsf.nyc@gmail.com>
2019-01-29 13:25:19 -0800

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" flask app web server listening on 0.0.0.0 on port 5000 """
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def hello():
+    """ Well Hello There """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ Just returns HBNB """
+    return "HBNB"
+
+
+app.run("0.0.0.0")
